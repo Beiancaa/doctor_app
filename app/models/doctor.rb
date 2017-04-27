800eeefc7b2135a49b30f662c4c52b64ec319bd3
Beianca <beiancayousif89@gmail.com>
2017-04-27 02:32:51 +0000

    validation

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,10 @@
 class Doctor < ActiveRecord::Base
 has_many :patients
+
+  validates_presence_of :license_number
+  validates_format_of :email, :with => (/^([^@\s]+)@((gmail)\.+[a-z]{2,})$/i), :multiline => true
+  validates :phone_number ,:presence => true,
+                 :numericality => true,
+                 :length => { :minimum => 11, :maximum => 11 }
+
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,11 @@
 class Patient < ActiveRecord::Base
   belongs_to :doctor
+  
+    validates_presence_of :first_name
+    validates_presence_of :last_name
+    validates_presence_of :doctor_id
+    validates_format_of :email, :with => (/^([^@\s]+)@((gmail)\.+[a-z]{2,})$/i), :multiline => true
+    validates :phone ,:presence => true,
+                 :numericality => true,
+                 :length => { :minimum => 11, :maximum => 11 }
 end
